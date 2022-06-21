19c3c62f6f1b393e0015a64c74325f9d5e8d44f2
Andrew Kulikov <jadecoresky@gmail.com>
2022-06-21 19:25:41 +0300

    chore: adding bribe handler

--- src/votium-bribe.ts
+++ src/votium-bribe.ts
@@ -1,78 +1,12 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  VotiumBribe,
-  Bribed,
-  Initiated,
-  ModifiedTeam,
-  OwnershipTransferred,
-  UpdatedDistributor,
-  UpdatedFee,
-  WhitelistRequirement,
-  Whitelisted
-} from "../generated/VotiumBribe/VotiumBribe"
-import { ExampleEntity } from "../generated/schema"
+import { Bribed } from "../generated/VotiumBribe/VotiumBribe"
+import { Bribe } from "../generated/schema"
 
-export function handleBribed(event: Bribed): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleDepositBribe(event: Bribed): void {
+  const bribe = new Bribe(event.transaction.hash);
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity._token = event.params._token
-  entity._amount = event.params._amount
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DENOMINATOR(...)
-  // - contract.approvedTeam(...)
-  // - contract.delegationHash(...)
-  // - contract.feeAddress(...)
-  // - contract.isWinningSignature(...)
-  // - contract.owner(...)
-  // - contract.platformFee(...)
-  // - contract.proposalInfo(...)
-  // - contract.requireWhitelist(...)
-  // - contract.tokenInfo(...)
+  bribe.proposal = event.params._proposal;
+  bribe.token = event.params._token;
+  bribe.amount = event.params._amount;
+  bribe.choiceIndex = event.params._choiceIndex;
+  bribe.save();
 }
-
-export function handleInitiated(event: Initiated): void {}
-
-export function handleModifiedTeam(event: ModifiedTeam): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleUpdatedDistributor(event: UpdatedDistributor): void {}
-
-export function handleUpdatedFee(event: UpdatedFee): void {}
-
-export function handleWhitelistRequirement(event: WhitelistRequirement): void {}
-
-export function handleWhitelisted(event: Whitelisted): void {}
